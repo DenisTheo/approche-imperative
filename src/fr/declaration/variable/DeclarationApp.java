6470624767820ce7da5054300e2712958dd06b37
DenisTheo <contact.denis.theo@gmail.com>
2023-10-04 14:18:37 +0200

    First class initialized with random variable declarations

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,17 @@
+package fr.declaration.variable;
+
+public class DeclarationApp
+{
+	public static void main(String[] args)
+	{
+		byte value_byte = 1;
+		short value_short = 4;
+		int value_integer = 64;
+		long value_long = 42_666_000;
+		float value_float = 3.14F;
+		double value_double = 3.14159;
+		boolean value_boolean = true;
+		char value_character = 'N';
+		String value_string = "Lorem Ipsum [...]";
+	}
+}
