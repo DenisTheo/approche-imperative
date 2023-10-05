a5d8a00b199d29c73ab97a42ebd91670c12ddc62
DenisTheo <contact.denis.theo@gmail.com>
2023-10-05 15:29:40 +0200

    added a complete InteractifTableMult Class

---
+++ src/fr/algorithmie/InteractifTableMult.java
@@ -0,0 +1,24 @@
+package fr.algorithmie;
+
+import java.util.Scanner;
+
+public class InteractifTableMult
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in) ;
+		int nb = 0;
+		
+		do
+		{// This part is exactly the same as the prior Exercise
+			System.out.println("Please enter a number between 1 and 10:");
+			nb = scanner.nextInt() ;
+		}while(nb < 1 || nb > 10);
+		System.out.println("You entered " + nb + ".\n");
+		
+		System.out.println("Table of " + nb + ":");
+		for(int i=1; i<=10; i++)// Simple Loop
+			System.out.println(nb + " x " + i + " = " + nb*i + ".");
+	}
+
+}
