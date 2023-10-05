b66a8b80497fc0c3a09babfac303c6952d521fe8
DenisTheo <contact.denis.theo@gmail.com>
2023-10-05 10:38:28 +0200

    added array ordered and reversed content printing on console

--- src/fr/algorithmie/AffichageInverse.java
+++ src/fr/algorithmie/AffichageInverse.java
@@ -2,5 +2,22 @@ package fr.algorithmie;
 
 public class AffichageInverse
 {
-	int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+	private static int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+
+	public static void main(String[] args)
+	{
+		System.out.println("args = {");
+		for(int i=0; i<array.length; i++)
+		{
+			System.out.println(array[i] + ' ');
+		}
+		System.out.println("};\n\n");
+
+		System.out.println("ordre inverse:\n");
+		for(int i=array.length-1; i>=0; i--)
+		{
+			System.out.println(array[i] + ' ');
+		}
+		System.out.println(".\n\n");
+	}
 }
