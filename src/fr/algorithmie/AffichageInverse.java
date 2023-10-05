ab8c83c45dab44ad469816cbf7a76ab95e5ddfdc
DenisTheo <contact.denis.theo@gmail.com>
2023-10-05 11:09:31 +0200

    fixedbreaking display bug and added comments in both classes

--- src/fr/algorithmie/AffichageInverse.java
+++ src/fr/algorithmie/AffichageInverse.java
@@ -6,26 +6,32 @@ public class AffichageInverse
 
 	public static void main(String[] args)
 	{
-		int[] arrayCopy = new int[array.length];
+		int[] arrayCopy = new int[array.length]; // Initialises arrayCopy to have the same length as array
 		
+		// Loop to display array's content. Should look like this:
+		// "array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4}."
 		System.out.print("array = {");
 		for(int i=0; i<array.length; i++)
 		{
-			arrayCopy[i] = array[i];
+			arrayCopy[i] = array[i]; // Also uses that loop to copy array's content into arrayCopy
 			System.out.print(array[i]);
 			
 			if(i < array.length-1)
-				System.out.print(', ');
+				System.out.print(", ");
 		}
-		System.out.print("};\n\n");
+		System.out.print("}.\n\n");
 
+		// Loop to display array's content. Should look like this:
+		// "ordre inverse:
+		// -4, 14, 0, 3, 2, 17, -1, 7, 28, -2, 4, 7, 8, 0, -3, 15, 1."
 		System.out.print("ordre inverse:\n");
 		for(int i=array.length-1; i>=0; i--)
 		{
 			System.out.print(array[i]);
 			
 			if(i > 0)
-				System.out.print(', ');
-		}		
+				System.out.print(", ");
+		}
+		System.out.print(".\n\n");
 	}
 }
--- src/fr/algorithmie/InversionContenu.java
+++ src/fr/algorithmie/InversionContenu.java
@@ -6,29 +6,35 @@ public class InversionContenu
 	
 	public static void main(String[] args)
 	{
-		int[] arrayCopy = new int[array.length];
+		int temp = array.length-1; // temporary variable mainly used for the copy to avoid calculating it multiple times
 		
+		// Loop to display array's content. Should look like this:
+		// "array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4}."
 		System.out.print("array = {");
 		for(int i=0; i<array.length; i++)
 		{
 			System.out.print(array[i]);
 			
-			if(i < array.length-1)
-				System.out.print(', ');
+			if(i < temp)
+				System.out.print(", ");
 		}
-		System.out.print("};\n\n");
-		
-		for(int i=array.length-1; i>=0; i--)
-			arrayCopy[i] = array[i];
+		System.out.print("}.\n\n");
 
+		int[] arrayCopy = new int[array.length]; // Initialises arrayCopy to have the same length as array
+		// Simple loop counting backwards to copy array's content into arrayCopy
+		for(int i=0; i<temp; i++)
+			arrayCopy[i] = array[temp-i]; // arrayCopy's slot ascending while array's descending
+
+		// Loop to display arrayCopys's content. Should look like this:
+		// "arrayCopy = {-4, 14, 0, 3, 2, 17, -1, 7, 28, -2, 4, 7, 8, 0, -3, 15, 1}."
 		System.out.print("arrayCopy = {");
 		for(int i=0; i<arrayCopy.length; i++)
 		{
 			System.out.print(arrayCopy[i]);
 			
-			if(i > 0)
-				System.out.print(', ');
+			if(i < temp) // Cheating a bit there, using array's length-1 instead of arrayCopy's -1, but they should be the same anyway
+				System.out.print(", ");
 		}
-		System.out.print("};\n\n");
+		System.out.print("}.\n\n");
 	}
 }
